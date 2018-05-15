39f604fb818e945bb3d65abe599ab33748335666
Fawaz <fawazsiddiqi@yahoo.com>
2018-05-15 13:24:50 +0400

    Initial Commit

---
+++ HashDistance/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HashDistance
+//
+//  Created by Fawaz Siddiqi on 5/15/18.
+//  Copyright © 2018 Fawaz Siddiqi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
